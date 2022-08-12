94ae57b79244eb4ec49cbbea2cbf8f3874a8e58d
NataliaOrlova <natakarelskaya@yandex.ru>
2022-08-12 14:07:26 +0400

    добавила решение task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -4,3 +4,16 @@
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("----------");
+Console.WriteLine(" n  | n^3 ");
+Console.WriteLine("----------");
+
+for (int i = 1; i <= n; i++)
+{
+  Console.WriteLine($"{i, 3} | {i*i*i, 4}");  
+}
+Console.WriteLine("----------");
