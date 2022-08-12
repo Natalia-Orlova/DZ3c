2daa6db03c118d1e604423f7a0ece91a47917fad
NataliaOrlova <natakarelskaya@yandex.ru>
2022-08-12 14:27:57 +0400

    добавила условие task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -5,15 +5,24 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+Console.Clear();
 Console.WriteLine("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("----------");
-Console.WriteLine(" n  | n^3 ");
-Console.WriteLine("----------");
-
-for (int i = 1; i <= n; i++)
+if (n < 0)
+    Console.WriteLine("Введено некорректное число, повторите попытку");
+else
 {
-  Console.WriteLine($"{i, 3} | {Math.Pow(i,3), 4}"); 
+    Console.WriteLine("----------");
+    Console.WriteLine(" n  | n^3 ");
+    Console.WriteLine("----------");
+
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine($"{i,3} | {Math.Pow(i, 3),4}");
+    }
+    Console.WriteLine("----------");
 }
-Console.WriteLine("----------");
+
+
+
