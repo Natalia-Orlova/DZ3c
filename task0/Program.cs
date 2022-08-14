e0fc2e7822e028f43d29a5e055a028909fe0a49d
NataliaOrlova <natakarelskaya@yandex.ru>
2022-08-14 19:57:07 +0400

    добавила решение task0

--- task0/Program.cs
+++ task0/Program.cs
@@ -5,3 +5,25 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Palindrome5(int num)
+{
+    if (num > 9999 && num <= 99999)
+    {
+        int temp1 = num / 1000;
+        int temp2 = num % 100;
+
+        if (temp1 / 10 == temp2 % 10 && temp1 % 10 == temp2 / 10)
+            Console.WriteLine($"Число {num} - палиндром");
+        else
+            Console.WriteLine($"Число {num} - не палиндром");
+    }
+    else
+        Console.WriteLine($"Число {num} не пятизначное, попробуйте еще раз");
+}
+
+Palindrome5(number);
